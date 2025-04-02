61562baa487fa031ab5d1e0d035c517e19704dc3
Shema Giscard <135936069+shemaiscard@users.noreply.github.com>
2025-04-03 01:24:27 +0900

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -214,12 +214,11 @@ if st.button("process") or url:
             'http_headers': {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
                 'Referer': 'https://www.google.com/'
-            }
-            
-        'forceip': 4,
-        'geo_bypass': True,
-        'geo_bypass_country': 'US',
-        'ignore_age_restrictions': True,
+            }            
+            'forceip': 4,
+            'geo_bypass': True,
+            'geo_bypass_country': 'US',
+            'ignore_age_restrictions': True,
     }
     
         try:
